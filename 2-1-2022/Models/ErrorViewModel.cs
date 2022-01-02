5831a1f1732c14b66dcd4bc38b93d8e63705e2ed
Khuất Tuấn Anhh <khuattuananh202002@gmail.com>
2022-01-02 09:05:28 +0700

    Add project files.

---
+++ 2-1-2022/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2_1_2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
